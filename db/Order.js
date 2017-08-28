8125fdd4fa90e8a646eed25a7b9c2a63ec2b069f
Burcu Noyan <burcunoyan@gmail.com>
2017-08-27 21:19:51 -0400

    move addProductToCart to db/Order.js

--- db/Order.js
+++ db/Order.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
 const conn = require('./conn');
-const LineItem = require('./LineItem');
 
 const Order = conn.define('order', {
   isCart: {
@@ -18,6 +17,45 @@ const Order = conn.define('order', {
 //     throw new Error('address required');
 //   }
 // });
+
+Order.addProductToCart = function(id) {
+  return conn.models.product.findById(id)
+    .then(product => {
+      return Order.findAll({ where: { isCart: true }})
+        .then(orders => {
+          if (!orders.length) {
+            return Order.create({ isCart: true })
+              .then(order => { return order });
+          }
+          else {
+            orders[0].isCart = true;
+            return orders[0].save();
+          }
+        })
+        .then(order => {
+          return conn.models.lineitem.findAll({ where: {
+              productId: product.id,
+              orderId: order.id
+            }})
+            .then(lineitem => {
+              if (lineitem.length) {
+                lineitem[0].quantity++;
+                return lineitem[0].save();
+              }
+              else {
+                return conn.models.lineitem.create({
+                    quantity: 1,
+                    orderId: order.id,
+                    productId: product.id
+                  })
+                  .then(lineitem => {
+                    return lineitem.save();
+                  })
+              }
+          })
+        })
+    })
+};
 
 Order.findOrderList = function() {
   return Order.findAll({
